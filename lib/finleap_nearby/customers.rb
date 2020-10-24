61a95725cdabe28da10b61e732f20514d23e00a9
Sajib Hassan <sajib.hassan@gmail.com>
2020-10-25 02:59:43 +0600

    Refactor customer class

--- lib/finleap_nearby/customers.rb
+++ lib/finleap_nearby/customers.rb
@@ -1,8 +1,10 @@
 module FinleapNearby
   class Customers
 
-    attr_reader :customers
-
+    ##
+    # Override the defaults value to match the customers
+    # Accept named parameters
+    #
     def initialize(search_radius: nil, search_radius_unit: nil, data_file_path: nil, center_point: nil)
       ::FinleapNearby.config_defaults
 
@@ -18,19 +20,9 @@ module FinleapNearby
       validate_params
     end
 
-
-    def calculate(sorted_by = "user_id")
-      filter_customers
-      sort_customers(sorted_by)
-
-      self
-    end
-
     def self.valid_keys?(customer)
-      (::FinleapNearby.configuration.customer_data_keys - customer.keys).empty?
+      (::FinleapNearby::Configuration::REQUIRED_DATA_KEYS - customer.keys).empty?
     end
-
-    private
 
     def validate_params
       raise ::FinleapNearby::InvalidParams, "Invalid search radius = #{@search_radius}" unless @search_radius > 0.0
@@ -38,25 +30,53 @@ module FinleapNearby
       raise ::FinleapNearby::InvalidFilePath, "Invalid file path = #{@data_file_path}" unless File.exist?(@data_file_path)
     end
 
+    ##
+    # Calculate the matched customers and sort by "user_id"
+    #
+    def filter_and_sort(sort_by = nil)
+      filter_customers
+      sort_customers(sort_by)
+
+      self
+    end
+
+    def customers(output_keys = nil)
+      output_keys ||= ::FinleapNearby.configuration.result_data_keys
+      @customers.map{|customer| customer.slice(*output_keys)}
+    end
+
+
     def filter_customers
       File.foreach(@data_file_path) do |customer|
         customer = JSON.parse(customer)
 
         raise ::FinleapNearby::RequiredDataMissing, "Required data key missing" unless ::FinleapNearby::Customers.valid_keys?(customer)
 
-        @customers << customer.slice("user_id", "name") if @search_radius >= calculate_distance(customer)
+        distance = calculate_distance(customer)
+        @customers << customer.merge({"distance" => distance}) if @search_radius >= distance
       end
+
+      self
     end
+
+    def sort_customers(sort_by)
+      sort_by ||= ::FinleapNearby.configuration.result_sort_by
+
+      return [] if @customers.empty?
+      raise ::FinleapNearby::InvalidSortKey, "Invalid sort key" unless @customers.first.has_key?(sort_by)
+
+      @customers.sort_by! { |obj| obj[sort_by.to_s] }
+
+      self
+    end
+
+    private
 
     def calculate_distance(customer)
       ::FinleapNearby::GeoCalculator.distance(@center_point,
                                               [customer["latitude"], customer["longitude"]],
                                               { units: @search_radius_unit }
-      )
-    end
-
-    def sort_customers(sort_by)
-      @customers.sort_by! { |obj| obj[sort_by.to_s] }
+      ).round(3)
     end
 
   end
